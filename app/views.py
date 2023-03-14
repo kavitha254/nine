0fea54d01151fdb0dcdcfe92f59b435adabc0e0b
kavitha254 <kavithakavithayadas@gmail.com>
2023-03-14 11:59:31 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def jinja_print(request):
+    d={'name':'kavi','age':21}
+    return render(request,'jinja_print.html',context=d)
